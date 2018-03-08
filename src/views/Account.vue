318f85a959d80f286eebdd1c9c3591daa812394e
wangzhencan <546866990@qq.com>
2018-03-08 11:33:11 +0800

    change order

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -9,13 +9,13 @@
                         </div>
                         <div class="box-inner">
                             <ul class="account-nav">
-                                <router-link tag="li" :to='{name:"Account"}' exact activeClass="current"><a>我的订单</a></router-link>
+                                <router-link tag="li" :to='{name:"Account"}' exact activeClass="current"><a>我的订单1</a></router-link>
                                 <router-link tag="li" :to='{name:"Address"}' activeClass="current"><a>收货地址</a></router-link>
                             </ul>
                         </div>
                     </div>
                 </div>
-                <router-view></router-view> 
+                <router-view></router-view>
             </div>
         </div>
     </div>
